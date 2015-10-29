8efc358736a3c2f504432fb9be300273bbb67868
Frank Mitchell <fmitchell@rapid7.com>
2015-10-29 16:32:00 -0400

    CORE-1109 Moves all the default layout bits into their own view.

--- views/refresh.jsx
+++ views/refresh.jsx
@@ -1,28 +1,16 @@
 var React = require('react')
+var DefaultLayout = require('./layouts/default')
 
 var Refresh = React.createClass({
   render: function() {
     return (
-<html lang="en">
-<head>
-<title>Awsaml</title>
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" />
-<link rel="stylesheet" href="/css/app.css" />
-</head>
-<body>
-<div className="container-fluid">
-<div className="row">
+<DefaultLayout title={this.props.title}>
 <div className="col-xs-9 col-centered widget">
 <p>Access Key: <span id="accessKey">{this.props.accessKey}</span></p>
 <p>Secret Key: <span id="secretKey">{this.props.secretKey}</span></p>
 <a className="btn btn-default" href="/refresh" role="button">Refresh</a>
 </div>
-</div>
-</div>
-</body>
-</html>
+</DefaultLayout>
     )
   }
 })
