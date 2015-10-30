352a5893282ba773ff5723c01a4e094aa8969f2a
Frank Mitchell <fmitchell@rapid7.com>
2015-10-30 16:07:36 -0400

    CORE-1109 Displays the session token in the UI.

--- views/refresh.jsx
+++ views/refresh.jsx
@@ -8,8 +8,9 @@ var Refresh = React.createClass({
 <div className="col-centered rounded-6 wrapper">
 <img className="logo" src="https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8" alt="Rapid7" />
 <div className="col-centered rounded-6 content">
-<p>Access Key: <span id="accessKey">{this.props.accessKey}</span></p>
-<p>Secret Key: <span id="secretKey">{this.props.secretKey}</span></p>
+<p>Access Key: {this.props.accessKey}</p>
+<p>Secret Key: {this.props.secretKey}</p>
+<p>Session Token: {this.props.sessionToken}</p>
 <a className="btn btn-default" href="/refresh" role="button">Refresh</a>
 </div>
 </div>
