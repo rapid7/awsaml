cee582751cf0f0fa9660214293e5e4b362122b50
Frank Mitchell <fmitchell@rapid7.com>
2015-10-30 10:03:10 -0400

    CORE-1109 Visual tweaks so content is centered.

--- views/refresh.jsx
+++ views/refresh.jsx
@@ -5,10 +5,13 @@ var Refresh = React.createClass({
   render: function() {
     return (
 <DefaultLayout title={this.props.title}>
-<div className="col-xs-9 col-centered widget">
+<div className="col-centered rounded-6 wrapper">
+<img className="logo" src="https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8" alt="Rapid7" />
+<div className="col-centered rounded-6 content">
 <p>Access Key: <span id="accessKey">{this.props.accessKey}</span></p>
 <p>Secret Key: <span id="secretKey">{this.props.secretKey}</span></p>
 <a className="btn btn-default" href="/refresh" role="button">Refresh</a>
+</div>
 </div>
 </DefaultLayout>
     )
