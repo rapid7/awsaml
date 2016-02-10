ca5ba8405b6d8119303943a262aef7816438fc33
Dave Greene <dgreene@rapid7.com>
2016-02-10 12:10:04 -0500

    Updated jsx files for ES6. Resolved linting errors.

--- views/configure.jsx
+++ views/configure.jsx
@@ -1,33 +1,53 @@
-var React = require('react')
-var DefaultLayout = require('./layouts/default')
+'use strict';
 
-var Configure = React.createClass({
-  render: function() {
-    var urlGroupClass = 'form-group'
+const React = require('react');
+const DefaultLayout = require('./layouts/default');
+
+const propTypes = {
+  error: React.PropTypes.bool.isRequired,
+  title: React.PropTypes.string.isRequired,
+  metadataUrl: React.PropTypes.string.isRequired
+};
+
+class Configure extends React.Component { // eslint-disable-line react/display-name
+  render() {
+    let urlGroupClass = 'form-group';
+
     if (this.props.error) {
-      urlGroupClass += ' has-error'
+      urlGroupClass += ' has-error';
     }
-
     return (
-<DefaultLayout title={this.props.title}>
-<div className="col-centered rounded-6 wrapper">
-<img className="logo" src="https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8" alt="Rapid7" />
-<div className="col-centered rounded-6 content">
-<form method="post">
-<fieldset>
-<legend>Configure</legend>
-<div className={urlGroupClass}>
-<label htmlFor="metadataUrl">SAML Metadata URL</label>
-<input className="form-control" id="metadataUrl" type="url" pattern="https://.+" required name="metadataUrl" defaultValue={this.props.metadataUrl} />
-</div>
-<button className="btn btn-default" type="submit">Done</button>
-</fieldset>
-</form>
-</div>
-</div>
-</DefaultLayout>
-    )
+      <DefaultLayout title={this.props.title}>
+        <div className='col-centered rounded-6 wrapper'>
+          <img alt='Rapid7'
+            className='logo'
+            src='https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8'
+          />
+          <div className='col-centered rounded-6 content'>
+            <form method='post'>
+              <fieldset>
+                <legend>Configure</legend>
+                <div className={urlGroupClass}>
+                  <label htmlFor='metadataUrl'>SAML Metadata URL</label>
+                  <input className='form-control'
+                    defaultValue={this.props.metadataUrl}
+                    id='metadataUrl'
+                    name='metadataUrl'
+                    pattern='https://.+'
+                    required
+                    type='url'
+                  />
+                </div>
+                <button className='btn btn-default' type='submit'>Done</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </DefaultLayout>
+    );
   }
-})
+}
 
-module.exports = Configure
+Configure.propTypes = propTypes;
+
+module.exports = Configure;
--- views/refresh.jsx
+++ views/refresh.jsx
@@ -1,23 +1,36 @@
-var React = require('react')
-var DefaultLayout = require('./layouts/default')
+const React = require('react');
+const DefaultLayout = require('./layouts/default');
 
-var Refresh = React.createClass({
-  render: function() {
+const propTypes = {
+  title: React.PropTypes.string.isRequired,
+  accountId: React.PropTypes.string.isRequired,
+  accessKey: React.PropTypes.string.isRequired,
+  secretKey: React.PropTypes.string.isRequired,
+  sessionToken: React.PropTypes.string.isRequired
+};
+
+class Refresh extends React.Component { // eslint-disable-line react/display-name
+  render() {
     return (
-<DefaultLayout title={this.props.title}>
-<div className="col-centered rounded-6 wrapper">
-<img className="logo" src="https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8" alt="Rapid7" />
-<div className="col-centered rounded-6 content">
-<p>Account ID: {this.props.accountId}</p>
-<p>Access Key: {this.props.accessKey}</p>
-<p>Secret Key: {this.props.secretKey}</p>
-<p>Session Token: {this.props.sessionToken}</p>
-<a className="btn btn-default" href="/refresh" role="button">Refresh</a>
-</div>
-</div>
-</DefaultLayout>
-    )
+      <DefaultLayout title={this.props.title}>
+        <div className='col-centered rounded-6 wrapper'>
+          <img alt='Rapid7'
+            className='logo'
+            src='https://rapid7.okta.com/bc/image/fileStoreRecord?id=fs011ume6fjY7HcEE0i8'
+          />
+          <div className='col-centered rounded-6 content'>
+            <p>Account ID: {this.props.accountId}</p>
+            <p>Access Key: {this.props.accessKey}</p>
+            <p>Secret Key: {this.props.secretKey}</p>
+            <p>Session Token: {this.props.sessionToken}</p>
+            <a className='btn btn-default' href='/refresh' role='button'>Refresh</a>
+          </div>
+        </div>
+      </DefaultLayout>
+    );
   }
-})
+}
 
-module.exports = Refresh
+Refresh.propTypes = propTypes;
+
+module.exports = Refresh;
