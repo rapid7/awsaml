890c67c38cbad78cce1a4f0a9f5fb6f422c18868
Dave Greene <dgreene@rapid7.com>
2016-02-10 12:15:39 -0500

    Updated code to ES6. Resolved linting issues.

--- app-menu.js
+++ app-menu.js
@@ -1,133 +1,129 @@
-var Menu = require('menu')
-var Application = require('app')
+'use strict';
 
-var template = [{
+const Menu = require('menu');
+const Application = require('app');
+
+const template = [{
   label: 'Edit',
   submenu: [{
     label: 'Undo',
     accelerator: 'CmdOrCtrl+Z',
     role: 'undo'
-  },{
+  }, {
     label: 'Redo',
     accelerator: 'Shift+CmdOrCtrl+Z',
     role: 'redo'
-  },{
+  }, {
     type: 'separator'
-  },{
+  }, {
     label: 'Cut',
     accelerator: 'CmdOrCtrl+X',
     role: 'cut'
-  },{
+  }, {
     label: 'Copy',
     accelerator: 'CmdOrCtrl+C',
     role: 'copy'
-  },{
+  }, {
     label: 'Paste',
     accelerator: 'CmdOrCtrl+V',
     role: 'paste'
-  },{
+  }, {
     label: 'Select All',
     accelerator: 'CmdOrCtrl+A',
     role: 'selectall'
   }]
-},{
+}, {
   label: 'View',
   submenu: [{
     label: 'Reload',
     accelerator: 'CmdOrCtrl+R',
-    click: function (item, focusedWindow) {
+    click(item, focusedWindow) {
       if (focusedWindow) {
-        focusedWindow.reload()
+        focusedWindow.reload();
       }
     }
-  },{
+  }, {
     label: 'Toggle Full Screen',
-    accelerator: (function () {
-      if (process.platform == 'darwin') {
-        return 'Ctrl+Command+F'
-      } else {
-       return 'F11'
-      }
-    })(),
-    click: function (item, focusedWindow) {
+    accelerator: (function a() {
+      return (process.platform === 'darwin') ? 'Ctrl+Command+F' : 'F11';
+    }()),
+    click(item, focusedWindow) {
       if (focusedWindow) {
-        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
       }
     }
-  },{
+  }, {
     label: 'Toggle Developer Tools',
-    accelerator: (function () {
-      if (process.platform == 'darwin') {
-        return 'Alt+Command+I'
-      } else {
-        return 'Ctrl+Shift+I'
-      }
-    })(),
-    click: function (item, focusedWindow) {
+    accelerator: (function a() {
+      return (process.platform === 'darwin') ? 'Alt+Command+I' : 'Ctrl+Shift+I';
+    }()),
+    click(item, focusedWindow) {
       if (focusedWindow) {
-        focusedWindow.toggleDevTools()
+        focusedWindow.toggleDevTools();
       }
     }
   }]
-},{
+}, {
   label: 'Window',
   role: 'window',
   submenu: [{
     label: 'Minimize',
     accelerator: 'CmdOrCtrl+M',
     role: 'minimize'
-  },{
+  }, {
     label: 'Close',
     accelerator: 'CmdOrCtrl+W',
     role: 'close'
   }]
-}]
+}];
 
-if (process.platform == 'darwin') {
-  var name = 'Awsaml'
+if (process.platform === 'darwin') {
+  const name = 'Awsaml';
+
   template.unshift({
     label: name,
     submenu: [{
-      label: 'About '+name,
+      label: `About ${name}`,
       role: 'about'
-    },{
+    }, {
       type: 'separator'
-    },{
+    }, {
       label: 'Services',
       role: 'services',
       submenu: []
-    },{
+    }, {
       type: 'separator'
-    },{
-      label: 'Hide '+name,
+    }, {
+      label: `Hide ${name}`,
       accelerator: 'Command+H',
       role: 'hide'
-    },{
+    }, {
       label: 'Hide Others',
       accelerator: 'Command+Shift+H',
       role: 'hideothers'
-    },{
+    }, {
       label: 'Show All',
       role: 'unhide'
-    },{
+    }, {
       type: 'separator'
-    },{
+    }, {
       label: 'Quit',
       accelerator: 'Command+Q',
-      click: function () {
-        Application.quit()
+      click() {
+        Application.quit();
       }
     }]
-  })
+  });
 
   // Window menu.
-  template[3].submenu.push({
+  template[3].submenu.push({ // eslint-disable-line rapid7/static-magic-numbers
     type: 'separator'
-  },{
+  }, {
     label: 'Bring All to Front',
     role: 'front'
-  })
+  });
 }
 
-var menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
+const menu = Menu.buildFromTemplate(template);
+
+Menu.setApplicationMenu(menu);
