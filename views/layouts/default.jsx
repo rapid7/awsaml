ea80e726a30146d9a050295fd9265af8f1d4be7e
Dave Greene <dgreene@rapid7.com>
2016-06-08 10:28:24 -0400

    Resolved issue where Maxcdn was requiring SRI with the crossorigin attribute.

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -12,11 +12,13 @@ const DefaultLayout = function render(props) {
         <title>{props.title}</title>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
         <link
+          crossOrigin='anonymous'
           href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'
           integrity='sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=='
           rel='stylesheet'
         />
         <link
+          crossOrigin='anonymous'
           href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css'
           integrity='sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX'
           rel='stylesheet'
