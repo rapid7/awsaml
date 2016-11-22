9ceccc65ee5183c44761cd28ef91008f7fb657ea
Erran Carey <me@errancarey.com>
2016-11-22 11:58:14 +0000

    Bump to Awsaml v1.4.0 in brew-cask formula

--- brew/cask/awsaml.rb
+++ brew/cask/awsaml.rb
@@ -1,10 +1,10 @@
 cask 'awsaml' do
-  version '1.3.0'
-  sha256 '4f4459551c3991aed287b02486b8bf0562f6d2fb99e45e93cbe1ee21bc099e0d'
+  version '1.4.0'
+  sha256 '3f3aa01510627eafead8a205dda2423b3f5c8b36afd4dfc777cdb5781b30fe6b'
 
   url "https://github.com/rapid7/awsaml/releases/download/v#{version}/awsaml-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/rapid7/awsaml/releases.atom',
-          checkpoint: 'e6d9e30a41aa77bd8cd9248e4f58076d6820f3f733b92a1fa84284d4119aa4b5'
+          checkpoint: '881468e03a4add3ccaac07636e2812a915b14cecd9c66025829e1b7290b98c53'
   name 'awsaml'
   homepage 'https://github.com/rapid7/awsaml'
   license :mit
