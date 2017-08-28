30059d7914836e5fa9af71e653a2c86ce5e79cdd
Frank Mitchell <me@frankmitchell.org>
2017-08-28 09:45:01 -0400

    Update version and appcast checkpoint for v1.5.0.

--- brew/cask/awsaml.rb
+++ brew/cask/awsaml.rb
@@ -1,10 +1,10 @@
 cask 'awsaml' do
-  version '1.4.0'
+  version '1.5.0'
   sha256 '3f3aa01510627eafead8a205dda2423b3f5c8b36afd4dfc777cdb5781b30fe6b'
 
   url "https://github.com/rapid7/awsaml/releases/download/v#{version}/awsaml-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/rapid7/awsaml/releases.atom',
-          checkpoint: '881468e03a4add3ccaac07636e2812a915b14cecd9c66025829e1b7290b98c53'
+          checkpoint: '3d2a662d80c619406bac6682158d8b89320b9d5359863cfbeddd574b00019e9a'
   name 'awsaml'
   homepage 'https://github.com/rapid7/awsaml'
   license :mit
