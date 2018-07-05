74efec0ac94546c66f46f946d9ab3a827a4727a5
Dave Greene <davepgreene@gmail.com>
2018-07-05 17:18:10 -0400

    Fix linting issues in packaging script.

--- packager.js
+++ packager.js
@@ -1,18 +1,24 @@
 const packager = require('electron-packager');
 
 const platforms = (process.env.PLATFORM) ? process.env.PLATFORM.split(',') : ['darwin', 'linux', 'win32'];
+const ignoreRegex = new RegExp('^\\/(' +
+  // Insert files/directories in this project that should be ignored.
+  // Note: This regex is required because electron-packager's `ignore` property only accepts a regex
+  // See: https://github.com/electron-userland/electron-packager/blob/master/readme.md
+  // specifically: `You can use --ignore to ignore files and folders via a regular expression (not a glob pattern).`
+  'dist|src|test|public|.editorconfig|.eslintrc|.gitignore|.travis.yml|CHANGELOG.md|README.md' +
+  ')(\\/|$)', 'g');
 
 packager({
-  dir: __dirname,
-  out: 'dist',
-  arch: 'x64',
-  ignore: /^\/(dist|src|test|public|.editorconfig|.eslintrc|.gitignore|.travis.yml|CHANGELOG.md|electron-wait-react.js|README.md)(\/|$)/g,
-  asar: true,
-  name: 'Awsaml',
-  platform: platforms,
-  // Mac Options
   appBundleId: 'com.rapid7.awsaml',
-  helperBundleId: 'com.rapid7.awsaml.helper'
+  arch: 'x64',
+  asar: true,
+  dir: __dirname,
+  helperBundleId: 'com.rapid7.awsaml.helper',
+  ignore: ignoreRegex,
+  name: 'Awsaml',
+  out: 'dist',
+  platform: platforms,
 }).catch((err) => {
   console.error(err);
 });
