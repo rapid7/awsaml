4ace23fa0a1ad8ef53a04f998ba11f58dd3c86df
Alan Sebastian <asebastian2@gmail.com>
2019-08-08 18:35:29 -0400

    Update brew cask for 2.2.1

--- brew/cask/awsaml.rb
+++ brew/cask/awsaml.rb
@@ -1,6 +1,6 @@
 cask 'awsaml' do
-  version '2.2.0'
-  sha256 'f9a0a74c3630cc6a53b5d68bb34cd545827f9541b6393046bfff88b1658de4ae'
+  version '2.2.1'
+  sha256 '616fde14a614c77220529a70fbcf172a6e8a2a8bbd802af9451b07a732cf9056'
 
   url "https://github.com/rapid7/awsaml/releases/download/v#{version}/awsaml-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/rapid7/awsaml/releases.atom'
