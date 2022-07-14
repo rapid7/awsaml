61ee0fba4154fa085a186c86bc0ae6ab11c29bc5
ddavies-r7 <108679540+ddavies-r7@users.noreply.github.com>
2022-07-14 17:14:06 +0100

    Add Universal Binary support for Apple Silicon
    This change allows for native execution of awsaml on ARM macs

--- packager.js
+++ packager.js
@@ -9,16 +9,27 @@ const ignoreRegex = new RegExp('^\\/(' +
   'dist|src|test|public|.editorconfig|.eslintrc|.gitignore|.travis.yml|CHANGELOG.md|README.md' +
   ')(\\/|$)', 'g');
 
-packager({
-  appBundleId: 'com.rapid7.awsaml',
-  arch: 'x64',
-  asar: true,
-  dir: __dirname,
-  helperBundleId: 'com.rapid7.awsaml.helper',
-  ignore: ignoreRegex,
-  name: 'Awsaml',
-  out: 'dist',
-  platform: platforms,
-}).catch((err) => {
-  console.error(err);
-});
+for (i in platforms) {
+
+  let __arch = 'x64';
+
+  // Allows MacOS builds for both x64 and ARM64 in one universal binary.
+  if (platforms[i] === 'darwin') {
+    __arch = 'universal';
+  }
+
+  // build a unique package
+  packager({
+    appBundleId: 'com.rapid7.awsaml',
+    arch: __arch,
+    asar: true,
+    dir: __dirname,
+    helperBundleId: 'com.rapid7.awsaml.helper',
+    ignore: ignoreRegex,
+    name: 'Awsaml',
+    out: 'dist',
+    platform: platforms[i],
+  }).catch((err) => {
+    console.error(err);
+  });
+}
