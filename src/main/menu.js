fbd581dc2a010ffd82ad4d440437eb3555f4100a
Dave Greene <David_Greene@rapid7.com>
2023-04-07 16:06:43 -0400

    Updated menu.

--- src/main/menu.js
+++ src/main/menu.js
@@ -3,92 +3,11 @@ const {
   app,
 } = require('electron');
 
-const template = [{
-  label: 'Edit',
-  submenu: [{
-    accelerator: 'CmdOrCtrl+Z',
-    label: 'Undo',
-    role: 'undo',
-  }, {
-    accelerator: 'Shift+CmdOrCtrl+Z',
-    label: 'Redo',
-    role: 'redo',
-  }, {
-    type: 'separator',
-  }, {
-    accelerator: 'CmdOrCtrl+X',
-    label: 'Cut',
-    role: 'cut',
-  }, {
-    accelerator: 'CmdOrCtrl+C',
-    label: 'Copy',
-    role: 'copy',
-  }, {
-    accelerator: 'CmdOrCtrl+V',
-    label: 'Paste',
-    role: 'paste',
-  }, {
-    accelerator: 'CmdOrCtrl+A',
-    label: 'Select All',
-    role: 'selectall',
-  }],
-}, {
-  label: 'View',
-  submenu: [{
-    accelerator: 'CmdOrCtrl+R',
-    click(item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.reload();
-      }
-    },
-    label: 'Reload',
-  }, {
-    accelerator: 'CmdOrCtrl+Shift+R',
-    click(item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.emit('reset');
-      }
-    },
-    label: 'Reset',
-  }, {
-    accelerator: (function a() {
-      return (process.platform === 'darwin') ? 'Ctrl+Command+F' : 'F11';
-    }()),
-    click(item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-      }
-    },
-    label: 'Toggle Full Screen',
-  }, {
-    accelerator: (function a() {
-      return (process.platform === 'darwin') ? 'Alt+Command+I' : 'Ctrl+Shift+I';
-    }()),
-    click(item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.toggleDevTools();
-      }
-    },
-    label: 'Toggle Developer Tools',
-  }],
-}, {
-  label: 'Window',
-  role: 'window',
-  submenu: [{
-    accelerator: 'CmdOrCtrl+M',
-    label: 'Minimize',
-    role: 'minimize',
-  }, {
-    accelerator: 'CmdOrCtrl+W',
-    label: 'Close',
-    role: 'close',
-  }],
-}];
+const isMac = process.platform === 'darwin';
+const name = 'Awsaml';
 
-if (process.platform === 'darwin') {
-  const name = 'Awsaml';
-
-  template.unshift({
+const template = [
+  ...(isMac ? [{
     label: name,
     submenu: [{
       label: `About ${name}`,
@@ -121,16 +40,96 @@ if (process.platform === 'darwin') {
       },
       label: 'Quit',
     }],
-  });
-
-  // Window menu.
-  template[3].submenu.push({
-    type: 'separator',
+  }] : []),
+  {
+    label: 'Edit',
+    submenu: [{
+      accelerator: 'CmdOrCtrl+Z',
+      label: 'Undo',
+      role: 'undo',
+    }, {
+      accelerator: 'Shift+CmdOrCtrl+Z',
+      label: 'Redo',
+      role: 'redo',
+    }, {
+      type: 'separator',
+    }, {
+      accelerator: 'CmdOrCtrl+X',
+      label: 'Cut',
+      role: 'cut',
+    }, {
+      accelerator: 'CmdOrCtrl+C',
+      label: 'Copy',
+      role: 'copy',
+    }, {
+      accelerator: 'CmdOrCtrl+V',
+      label: 'Paste',
+      role: 'paste',
+    }, {
+      accelerator: 'CmdOrCtrl+A',
+      label: 'Select All',
+      role: 'selectall',
+    }],
   }, {
-    label: 'Bring All to Front',
-    role: 'front',
-  });
-}
+    label: 'View',
+    submenu: [{
+      accelerator: 'CmdOrCtrl+R',
+      click(item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.reload();
+        }
+      },
+      label: 'Reload',
+    }, {
+      accelerator: 'CmdOrCtrl+Shift+R',
+      click(item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.emit('reset');
+        }
+      },
+      label: 'Reset',
+    }, {
+      accelerator: (function a() {
+        return (process.platform === 'darwin') ? 'Ctrl+Command+F' : 'F11';
+      }()),
+      click(item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      label: 'Toggle Full Screen',
+    },
+    {
+      accelerator: (function a() {
+        return (process.platform === 'darwin') ? 'Alt+Command+I' : 'Ctrl+Shift+I';
+      }()),
+      click(item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      label: 'Toggle Developer Tools',
+    }],
+  }, {
+    label: 'Window',
+    role: 'window',
+    submenu: [{
+      accelerator: 'CmdOrCtrl+M',
+      label: 'Minimize',
+      role: 'minimize',
+    }, {
+      accelerator: 'CmdOrCtrl+W',
+      label: 'Close',
+      role: 'close',
+    },
+    ...(isMac ? [{
+      type: 'separator',
+    }, {
+      label: 'Bring All to Front',
+      role: 'front',
+    }] : [])],
+  },
+];
 
 const menu = Menu.buildFromTemplate(template);
 
