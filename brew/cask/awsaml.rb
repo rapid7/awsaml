2e29752b18c18c79af81c94b60332586407e37a6
Marguerite Martinez <84595842+mmurrell-r7@users.noreply.github.com>
2023-09-13 11:17:08 -0400

    Update awsaml.rb

--- brew/cask/awsaml.rb
+++ brew/cask/awsaml.rb
@@ -1,5 +1,5 @@
 cask "awsaml" do
-  version "3.1.0"
+  version "3.0.0"
   sha256 "b6336c92d518108469c0b2302a7da0f9c3ae7cc9916b035edf10fbaf70ae9104"
 
   url "https://github.com/rapid7/awsaml/releases/download/v#{version}/Awsaml-darwin-universal-#{version}.zip"
