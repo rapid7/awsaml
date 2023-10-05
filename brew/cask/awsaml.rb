ad48e4b2f9e02a3b1b8e086db151173e7c65682a
jenkins-pd <jenkins-pd@build-production.r7ops.com>
2023-10-05 17:53:57 +0000

    New patch release - v3.1.2

--- brew/cask/awsaml.rb
+++ brew/cask/awsaml.rb
@@ -1,5 +1,5 @@
 cask "awsaml" do
-  version "3.1.1"
+  version "3.1.2"
   sha256 "b6336c92d518108469c0b2302a7da0f9c3ae7cc9916b035edf10fbaf70ae9104"
 
   url "https://github.com/rapid7/awsaml/releases/download/v#{version}/Awsaml-darwin-universal-#{version}.zip"
